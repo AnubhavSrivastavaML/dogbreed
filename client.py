ebfd06bb69372b8b40c714a7d0862e6f2ae9f0eb
Anubhav <anubhavsrivastva96@gmail.com>
2021-03-15 07:34:30 +0530

    Updated With model Accuracy

---
+++ client.py
@@ -0,0 +1,18 @@
+import requests
+from flask import jsonify
+import base64
+import json
+
+url = 'http://localhost:5000/im_size'
+image_file = '/home/anubhav/Competitions/dog_breed/test/0a0b97441050bba8e733506de4655ea1.jpg'
+image = open(image_file, 'rb')
+image_read = image.read()
+image_64_encode = base64.b64encode(image_read)
+dic = {'image': image_64_encode.decode('utf-8')}
+my_img = json.dumps(dic)
+
+r = requests.post(url, json=my_img)
+
+
+print(r.json())
+
